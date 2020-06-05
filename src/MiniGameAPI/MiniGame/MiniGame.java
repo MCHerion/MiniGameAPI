b35bfb48fbe809a9f772b6d121ec97d1d6c4b900
Elytes <elytes@github.com>
2020-06-05 12:10:48 +0200

    Changes

--- src/MiniGameAPI/MiniGame/MiniGame.java
+++ src/MiniGameAPI/MiniGame/MiniGame.java
@@ -8,13 +8,7 @@ import MiniGameAPI.CustomPlayer.CustomPlayer;
 
 public class MiniGame<P extends CustomPlayer>
 {
-	protected GameState<MiniGame<? extends P>> _gameState;
 	protected ArrayList<Team<P>> _teams;
-	
-	public MiniGame(GameState<MiniGame<? extends P>> gameState)
-	{
-		setGameState(gameState);
-	}
 	
 	public ArrayList<P> getCustomPlayers()
 	{
@@ -24,17 +18,5 @@ public class MiniGame<P extends CustomPlayer>
 	public ArrayList<Player> getPlayers()
 	{
 		return null;
-	}
-	
-	public void setGameState(GameState<MiniGame<? extends P>> gameState)
-	{
-		_gameState.unsubscribe(this);
-		_gameState = gameState;
-		_gameState.subscribe(this);
-	}
-	
-	public GameState<MiniGame<? extends P>> getGameState()
-	{
-		return _gameState;
 	}
 }
