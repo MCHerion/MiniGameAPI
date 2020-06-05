d932481581a7c881fbe4f468d3fabc501694c5e6
Elytes <elytes@github.com>
2020-06-05 19:19:26 +0200

    Changes

--- src/MiniGameAPI/CustomPlayer/CustomPlayer.java
+++ src/MiniGameAPI/CustomPlayer/CustomPlayer.java
@@ -3,11 +3,30 @@ package MiniGameAPI.CustomPlayer;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
-public interface CustomPlayer
+import MiniGameAPI.MiniGame.MiniGame;
+
+public abstract class CustomPlayer
 {
-	public String getName();
+	protected MiniGame<?> _miniGame;
 	
-	public default Player getPlayer()
+	public MiniGame<?> getMiniGame()
+	{
+		return _miniGame;
+	}
+	
+	public void playMiniGame(MiniGame<?> miniGame)
+	{
+		_miniGame = miniGame;
+	}
+	
+	public void leaveMiniGame()
+	{
+		
+	}
+	
+	public abstract String getName();
+	
+	public Player getPlayer()
 	{
 		return Bukkit.getPlayer(getName());
 	}
---
+++ src/MiniGameAPI/MiniGame/MiniGameHandler.java
@@ -0,0 +1,6 @@
+package MiniGameAPI.MiniGame;
+
+public interface MiniGameHandler<MG extends MiniGame<?>>
+{
+	public MG getMiniGame();
+}
