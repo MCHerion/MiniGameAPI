e62c6f54156e544f3c48fe4e589e9d8a17265f0d
Elytes <elytes@github.com>
2020-06-05 14:03:03 +0200

    Update MiniGame.java

--- src/MiniGameAPI/MiniGame/MiniGame.java
+++ src/MiniGameAPI/MiniGame/MiniGame.java
@@ -2,13 +2,25 @@ package MiniGameAPI.MiniGame;
 
 import java.util.ArrayList;
 
+import org.bukkit.World;
 import org.bukkit.entity.Player;
 
 import MiniGameAPI.CustomPlayer.CustomPlayer;
 
 public class MiniGame<P extends CustomPlayer>
 {
+	protected World _world;
 	protected ArrayList<Team<P>> _teams;
+	
+	public MiniGame(World world)
+	{
+		_world = world;
+	}
+	
+	public World getWorld()
+	{
+		return _world;
+	}
 	
 	public ArrayList<P> getCustomPlayers()
 	{
