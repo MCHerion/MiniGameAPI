b26a1ae1fb41598fd9596dff7800c14fc3a4017d
Elytes <elytes@github.com>
2020-06-08 15:19:08 +0200

    Changes

--- src/MiniGameAPI/MiniGame/MiniGame.java
+++ src/MiniGameAPI/MiniGame/MiniGame.java
@@ -6,19 +6,32 @@ import org.bukkit.Bukkit;
 import org.bukkit.GameMode;
 import org.bukkit.World;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.potion.PotionEffect;
 
 import MiniGameAPI.CustomEvents.PlayerJoinMiniGameEvent;
 import MiniGameAPI.CustomPlayer.CustomPlayer;
 
-public abstract class MiniGame<P extends CustomPlayer<?>>
+public abstract class MiniGame<P extends CustomPlayer<?>> implements Listener
 {
 	protected World _world;
 	protected ArrayList<P> _players = new ArrayList<P>();
-	protected ArrayList<P> _spectators = new ArrayList<P>();
+	protected ArrayList<Player> _spectators = new ArrayList<Player>();
 	
 	public MiniGame(World world)
 	{
 		_world = world;
+	}
+	
+	@EventHandler
+	public void onPlayerQuit(PlayerQuitEvent event)
+	{
+		if(getSpectators().contains(event.getPlayer()))
+		{
+			removeSpectator(event.getPlayer());
+		}
 	}
 	
 	public World getWorld()
@@ -31,7 +44,7 @@ public abstract class MiniGame<P extends CustomPlayer<?>>
 		return _players;
 	}
 	
-	public ArrayList<P> getSpectators()
+	public ArrayList<Player> getSpectators()
 	{
 		return _spectators;
 	}
@@ -41,24 +54,34 @@ public abstract class MiniGame<P extends CustomPlayer<?>>
 		_players.add(player);
 		player.playMiniGame(this);
 		player.getPlayer().teleport(_world.getSpawnLocation());
+		player.getPlayer().setGameMode(GameMode.SURVIVAL);
+		for(PotionEffect effect : player.getPlayer().getActivePotionEffects())
+		{
+			player.getPlayer().removePotionEffect(effect.getType());
+		}
 		Bukkit.getPluginManager().callEvent(new PlayerJoinMiniGameEvent(this, player.getPlayer()));
 	}
 	
 	public void removePlayer(P player)
 	{
 		_players.remove(player);
+		_spectators.remove(player.getPlayer());
+	}
+	
+	public void removeSpectator(Player player)
+	{
+		_spectators.remove(player);
 	}
 	
 	public void lose(P player)
 	{
 		removePlayer(player);
-		joinAsSpectator(player);
 	}
 	
-	public void joinAsSpectator(P player)
+	public void joinAsSpectator(Player player)
 	{
 		_spectators.add(player);
-		player.getPlayer().setGameMode(GameMode.SPECTATOR);
+		player.setGameMode(GameMode.SPECTATOR);
 	}
 	
 	public ArrayList<Player> getPlayers()
@@ -74,10 +97,7 @@ public abstract class MiniGame<P extends CustomPlayer<?>>
 	public ArrayList<Player> getAllPlayers()
 	{
 		ArrayList<Player> allPlayers = new ArrayList<Player>();
-		for(P player : _spectators)
-		{
-			allPlayers.add(player.getPlayer());
-		}
+		allPlayers.addAll(_spectators);
 		for(P player : _players)
 		{
 			allPlayers.add(player.getPlayer());
